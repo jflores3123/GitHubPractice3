9d0661f83f0861971cdf0d86d254535c196e36c6
Will Schroeder <wschroeder5332@stu.d214.org>
2022-11-21 11:11:21 -0600

    Coded table view

--- GitHubPractice3/ViewController.swift
+++ GitHubPractice3/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textField : UITextField!
+    
+        var array : [String] = ["a", "b", "c"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       tableView.dataSource = self
+        
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        cell.textLabel?.text = array[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
 
 }
 
